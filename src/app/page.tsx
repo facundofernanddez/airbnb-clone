713d5cd6364eda27575f4745c339e271e418c2de
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-03-20 23:56:17 -0300

    add filter imgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import MapFilterItems from "@/components/MapFilterItems";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="container mx-auto px-5 lg:px-10">
       <MapFilterItems />
     </main>
   );
--- src/components/MapFilterItems.tsx
+++ src/components/MapFilterItems.tsx
@@ -1,3 +1,26 @@
+import { categoryItems } from "@/lib/categoryItems.ts";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function MapFilterItems() {
-  return <h1>Hello from filter items</h1>;
+  return (
+    <div>
+      {categoryItems.map((item, idx) => (
+        <Link
+          key={item.id}
+          href=""
+        >
+          <div className="relative w-6 h-6">
+            <Image
+              src={item.imageUrl}
+              alt="Category image"
+              className="w-6 h-6"
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
