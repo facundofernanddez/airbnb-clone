86f5e2a85851091325cd65b6434ff4e692986f79
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-03-22 07:36:56 -0300

    create the hooks to use

--- src/components/MapFilterItems.tsx
+++ src/components/MapFilterItems.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import { categoryItems } from "@/lib/categoryItems.ts";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 
 export default function MapFilterItems() {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("filter");
+  const pathname = usePathname();
+
+  const createQueryString = useCallback((name: string, value: string) => {});
+
   return (
     <div className="flex gap-x-10 mt-5 w-full overflow-x-scroll no-scrollbar">
       {categoryItems.map((item, idx) => (
