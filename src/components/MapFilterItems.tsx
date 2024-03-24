6aaa89a4da71a0e51eb724cff6d23919a458ce68
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-03-23 21:36:55 -0300

    looks better the mapfilter nav

--- src/components/MapFilterItems.tsx
+++ src/components/MapFilterItems.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { categoryItems } from "@/lib/categoryItems.ts";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
@@ -11,14 +12,29 @@ export default function MapFilterItems() {
   const search = searchParams.get("filter");
   const pathname = usePathname();
 
-  const createQueryString = useCallback((name: string, value: string) => {});
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
 
   return (
     <div className="flex gap-x-10 mt-5 w-full overflow-x-scroll no-scrollbar">
       {categoryItems.map((item, idx) => (
         <Link
           key={item.id}
-          href=""
+          href={pathname + "?" + createQueryString("filter", item.name)}
+          className={cn(
+            search === item.name
+              ? "border-b-2 border-black pb-2 flex-shrink-0"
+              : "opacity-70 flex-shrink-0",
+            "flex flex-col gap-y-3 items-center"
+          )}
         >
           <div className="relative w-6 h-6">
             <Image
