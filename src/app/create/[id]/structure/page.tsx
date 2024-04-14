f0b75d0881b78ca9bb360a0265e066efb52ef672
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-04-14 20:15:19 -0300

    start to build structure page

---
+++ src/app/create/[id]/structure/page.tsx
@@ -0,0 +1,11 @@
+export default function StructurePage() {
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors">
+          A ver veamos como funciona
+        </h2>
+      </div>
+    </>
+  );
+}
