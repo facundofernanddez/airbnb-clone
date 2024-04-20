19d8853fc139216816fa81d48cc328dcbd48a436
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-04-20 19:24:36 -0300

    add title

--- src/components/SelectedCategory.tsx
+++ src/components/SelectedCategory.tsx
@@ -3,10 +3,13 @@
 import { categoryItems } from "@/lib/categoryItems";
 import { Card, CardHeader } from "./ui/card";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function SelectCategory() {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-8 mt-10 w-3/5 mx-auto">
       {categoryItems.map((item) => (
         <div
           key={item.id}
@@ -21,6 +24,8 @@ export default function SelectCategory() {
                 width={32}
                 className="h-8 w-8"
               />
+
+              <h3 className="font-medium">{item.title}</h3>
             </CardHeader>
           </Card>
         </div>
