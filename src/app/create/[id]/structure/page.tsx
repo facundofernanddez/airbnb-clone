0ee7ca3f3615aa39782cb45e63f20fb61e0e779d
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-04-24 10:15:38 -0300

    create cancel button

--- src/app/create/[id]/structure/page.tsx
+++ src/app/create/[id]/structure/page.tsx
@@ -1,4 +1,5 @@
 import SelectCategory from "@/components/SelectedCategory";
+import { Button } from "@/components/ui/button";
 
 export default function StructurePage() {
   return (
@@ -11,6 +12,12 @@ export default function StructurePage() {
 
       <form>
         <SelectCategory />
+
+        <div className="fixed w-full bottom-0 z-10 bg-white border-t h-24">
+          <div className="flex items-center justify-between mx-auto px-5 lg:px-10 h-full">
+            <Button>Cancel</Button>
+          </div>
+        </div>
       </form>
     </>
   );
