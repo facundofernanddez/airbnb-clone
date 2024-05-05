92ce4a56f2ee8785e42047095e0b83879b5ce275
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-05-05 16:41:58 -0300

    change the default value to the state

--- src/components/SelectedCategory.tsx
+++ src/components/SelectedCategory.tsx
@@ -6,7 +6,9 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function SelectCategory() {
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string | undefined>(
+    undefined
+  );
 
   return (
     <div className="grid grid-cols-4 gap-8 mt-10 w-3/5 mx-auto mb-36">
