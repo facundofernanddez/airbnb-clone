8140bfd5300d9b26af50c41d60ad2ee27811b9c9
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-05-11 18:54:00 -0300

    update description page

--- src/app/create/[id]/description/page.tsx
+++ src/app/create/[id]/description/page.tsx
@@ -1,3 +1,17 @@
 export default function DescriptionPage() {
-  return <div>Description</div>;
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors">
+          Please describe your home as good as you can!
+        </h2>
+      </div>
+
+      <form>
+        <div className="mx-auto w-3/5 mt-10 flex flex-col gap-y-5 mb-36">
+          <div className="flex flex-col gap-y-2"></div>
+        </div>
+      </form>
+    </>
+  );
 }
