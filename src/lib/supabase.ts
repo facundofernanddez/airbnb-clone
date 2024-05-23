eb0c669b13867819b488f411fe9e4b95b1560dab
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-05-23 09:46:03 -0300

    add supabase url and anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  "https://xyzcompany.supabase.co",
-  "public-anon-key"
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_ANON_KEY as string
 );
