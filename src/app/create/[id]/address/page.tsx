d47b2d1f30b530d5297db866f8ce118ae179fe84
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-05-26 16:18:19 -0300

    add title

--- src/app/create/[id]/address/page.tsx
+++ src/app/create/[id]/address/page.tsx
@@ -1,3 +1,11 @@
 export default function AddressRoute() {
-  return <h1>Adress Route</h1>;
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors mb-10">
+          Where is your home located?
+        </h2>
+      </div>
+    </>
+  );
 }
