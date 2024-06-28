2757fa9ecec2d8f7d096c50a1992a3e925430505
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-06-28 10:03:05 -0300

    start to create map component

---
+++ src/components/Map.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+
+export default function Map() {
+  return (
+    <MapContainer
+      scrollWheelZoom={false}
+      className="h-[50vh] rounded-lg relative z-0"
+      center={[52.505, -0.09]}
+    ></MapContainer>
+  );
+}
