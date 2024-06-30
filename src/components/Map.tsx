0c78919e70dc55b4571cff4f4fedcd5d00b33565
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-06-30 19:06:02 -0300

    create map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default function Map() {
   return (
@@ -8,6 +9,12 @@ export default function Map() {
       scrollWheelZoom={false}
       className="h-[50vh] rounded-lg relative z-0"
       center={[52.505, -0.09]}
-    ></MapContainer>
+      zoom={13}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
   );
 }
