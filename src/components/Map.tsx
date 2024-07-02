ed654be184e092f428673ba4c7007619862d7238
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-07-02 10:29:51 -0300

    pass location value and transform to latlang

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,13 +2,17 @@
 
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useCountries } from "@/lib/getCountries";
 
-export default function Map() {
+export default function Map({ locationValue }: { locationValue: string }) {
+  const { getCountryByValue } = useCountries();
+  const latLang = getCountryByValue(locationValue)?.latLang;
+
   return (
     <MapContainer
       scrollWheelZoom={false}
       className="h-[50vh] rounded-lg relative z-0"
-      center={[52.505, -0.09]}
+      center={latLang ?? [52.505, -0.09]}
       zoom={13}
     >
       <TileLayer
