789467930bf76754b7c5eeeb2d3fb96945952fa2
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-07-06 12:03:09 -0300

    add position marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,6 +3,13 @@
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useCountries } from "@/lib/getCountries";
+import { icon } from "leaflet";
+
+const ICON = icon({
+  iconUrl:
+    "https://images.vexels.com/media/users/3/131261/isolated/preview/b2e48580147ca0ed3f970f30bf8bb009-map-location-marker-by-vexels.png",
+  iconSize: [50, 50],
+});
 
 export default function Map({ locationValue }: { locationValue: string }) {
   const { getCountryByValue } = useCountries();
@@ -13,11 +20,16 @@ export default function Map({ locationValue }: { locationValue: string }) {
       scrollWheelZoom={false}
       className="h-[50vh] rounded-lg relative z-0"
       center={latLang ?? [52.505, -0.09]}
-      zoom={13}
+      zoom={8}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <Marker
+        position={latLang ?? [52.505, -0.09]}
+        icon={ICON}
       />
     </MapContainer>
   );
