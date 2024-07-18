b3a71bf479ab9dc71ab6fa6d9d9b7eb2fe265505
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-07-17 22:51:06 -0300

    create async function and render data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
+import ListingCard from "@/components/ListingCard";
 import MapFilterItems from "@/components/MapFilterItems";
+import prisma from "@/lib/db";
 
-export default function Home() {
+async function getData() {
+  const data = await prisma.home.findMany({
+    where: {
+      addedCategory: true,
+      addedLocation: true,
+      addedDescription: true,
+    },
+    select: {
+      photo: true,
+      id: true,
+      price: true,
+      description: true,
+    },
+  });
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <main className="container mx-auto px-5 lg:px-10">
       <MapFilterItems />
+
+      <div>
+        {data.map((item) => (
+          <ListingCard key={item.id} />
+        ))}
+      </div>
     </main>
   );
 }
