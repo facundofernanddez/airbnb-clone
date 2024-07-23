1c3e846f0aa6ebcaf693493b9693d7269ca5620f
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-07-22 22:04:04 -0300

    add images host

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         protocol: "https",
         port: "",
       },
+      {
+        hostname: "wgqklerajuawttphkbqd.supabase.co",
+        protocol: "https",
+        port: "",
+      },
     ],
   },
 };
