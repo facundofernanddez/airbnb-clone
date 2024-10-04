11865e52968a3bba259b69df5cb6f509f08950d0
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-10-03 21:44:32 -0300

    add to favorite action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -121,3 +121,15 @@ export async function createLocation(formData: FormData) {
 
   return redirect("/");
 }
+
+export async function addToFavorite(formData: FormData) {
+  const homeId = formData.get("homeId") as string;
+  const userId = formData.get("userId") as string;
+
+  const data = await prisma.favorite.create({
+    data: {
+      id: "",
+      userId: "",
+    },
+  });
+}
