b3d167017505909b9e7f0a5d8fd8ea954d4a901e
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-10-12 17:02:01 -0300

    render your homes

--- src/app/my-homes/page.tsx
+++ src/app/my-homes/page.tsx
@@ -1,3 +1,5 @@
+import ListingCard from "@/components/ListingCard";
+import NoItems from "@/components/NoItem";
 import prisma from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
@@ -40,6 +42,30 @@ export default async function MyHomes() {
   return (
     <section className="container mx-auto px-5 lg:px-10 mt-10">
       <h2 className="text-3xl font-semibold tracking-tight">Your Homes</h2>
+
+      {data.length === 0 ? (
+        <NoItems
+          title="You dont have any Homes listed"
+          description="Please list a home on airbnb so that you can see right here"
+        />
+      ) : (
+        <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1 gap-8 mt-8">
+          {data.map((item) => (
+            <ListingCard
+              key={item.id}
+              imagePath={item.photo as string}
+              homeId={item.id}
+              price={item.price as number}
+              description={item.description as string}
+              location={item.country as string}
+              userId={item.id}
+              pathName="/my-homes"
+              favoriteId={item.Favorite[0]?.id}
+              isInFavoriteList={item.Favorite.length > 0 ? true : false}
+            />
+          ))}
+        </div>
+      )}
     </section>
   );
 }
