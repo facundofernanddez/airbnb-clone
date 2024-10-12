40e779346e24dbf071d95619d539e6da4f831dd6
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-10-12 14:53:30 -0300

    get user session

--- src/app/my-homes/page.tsx
+++ src/app/my-homes/page.tsx
@@ -1,4 +1,6 @@
 import prisma from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 async function getData(userId: string) {
   const data = await prisma.home.findMany({
@@ -28,7 +30,13 @@ async function getData(userId: string) {
   return data;
 }
 
-export default function MyHomes() {
+export default async function MyHomes() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) return redirect("/");
+
+  const data = await getData(user.id);
   return (
     <section className="container mx-auto px-5 lg:px-10 mt-10">
       <h2 className="text-3xl font-semibold tracking-tight">Your Homes</h2>
