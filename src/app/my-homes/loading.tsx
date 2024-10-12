2a935f8d31a6723b7b4082145b38b3b45b68c687
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-10-12 20:00:49 -0300

    change name of component

--- src/app/my-homes/loading.tsx
+++ src/app/my-homes/loading.tsx
@@ -1,6 +1,6 @@
 import { SkeletonCard } from "@/components/SkeletonCard";
 
-export default function FavoritesLoading() {
+export default function HomesLoadingPage() {
   return (
     <section className="container mx-auto px-5 lg:px-10 mt-10">
       <h2 className="text-3xl font-semibold tracking-tight">Your Favorites</h2>
