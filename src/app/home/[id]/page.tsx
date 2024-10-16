5131cd10b81d179a094e816d5d063e4c50781319
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-10-16 08:22:08 -0300

    start to render individual home

--- src/app/home/[id]/page.tsx
+++ src/app/home/[id]/page.tsx
@@ -1,4 +1,5 @@
 import prisma from "@/lib/db";
+import { useCountries } from "@/lib/getCountries";
 import Image from "next/image";
 
 async function getData(homeId: string) {
@@ -15,6 +16,7 @@ async function getData(homeId: string) {
       bathrooms: true,
       title: true,
       categoryName: true,
+      country: true,
     },
   });
 
@@ -27,6 +29,9 @@ export default async function HomeRoute({
   params: { id: string };
 }) {
   const data = await getData(params.id);
+  const { getCountryByValue } = useCountries();
+  const country = getCountryByValue(data?.country as string);
+
   return (
     <div className="w-[75%] mx-auto mt-10">
       <h1 className="font-medium text-2xl mb-5">{data?.title}</h1>
@@ -38,6 +43,19 @@ export default async function HomeRoute({
           fill
           className="rounded-lg h-full object-cover w-full"
         />
+      </div>
+
+      <div className="flex justify-between gap-x-24 mt-8">
+        <div className="w-2/3">
+          <h3 className="text-xl font-medium">
+            {country?.flag} {country?.label} / {country?.region}
+          </h3>
+
+          <div className="flex gap-x-2 text-muted-foreground">
+            <p>{data?.guest} Guest</p> * <p>{data?.bedrooms} Bedrooms</p> *
+            <p>{data?.bathrooms} Bathrooms</p>
+          </div>
+        </div>
       </div>
     </div>
   );
